d4105b8a4d296e066c1021cc20390c5777e626f4
Maciek <46852030+recostudio@users.noreply.github.com>
2022-10-27 16:53:19 +0200

    Initial Commit

---
+++ SwiftUIVideoPreview/SwiftUIVideoPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIVideoPreviewApp.swift
+//  SwiftUIVideoPreview
+//
+//  Created by Maciej Szostak on 27/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIVideoPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
